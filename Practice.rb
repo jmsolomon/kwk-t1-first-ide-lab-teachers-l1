750f750f6f7ab6eb8e17377e817571fafc6e84ea
Jason Solomon <jasonmaxsolomon@gmail.com>
2018-07-18 14:13:23 +0000

    automatically backed up by learn

---
+++ Practice.rb
@@ -0,0 +1,18 @@
+# shopping_array = ["24k magic CD", "crocs", "potato", "ACT prep book"]
+# price = [15.98, 35, 0.8, 15]
+# shopping_hash = {}
+# items_price = 0 
+# shopping_array.each do |each_items|
+#   shopping_hash[each_items] = price[items_price]
+#   items_price += 1 
+# end
+
+your_name= "Jason"
+
+def say_hello
+  puts "Hello#{your_name}!"
+end
+
+say_hello
+
+
